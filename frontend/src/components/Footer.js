eeb23fcb9d21202d55c98bce5a3607d3a7455e55
Evaldas Pranckevicius <prevaldas@yahoo.com>
2022-07-02 20:11:02 +0100

    minor change

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
-      <p className="name">{currentYear} © Evaldas Pranckevicius</p>
+      <p className="name">{currentYear} ⚙︎ Evaldas Pranckevicius</p>
     </footer>
   );
 }
